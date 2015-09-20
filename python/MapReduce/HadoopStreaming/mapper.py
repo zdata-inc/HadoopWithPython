aaf793a5c673515eb6bce6287949535b530b27a8
Zachary Radtka <zachary.radtka@gmail.com>
2015-09-20 16:37:31 +0200

    Added some comments

--- python/MapReduce/HadoopStreaming/mapper.py
+++ python/MapReduce/HadoopStreaming/mapper.py
@@ -1,8 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
 import sys
 
+# Read each line from STDIN
 for line in sys.stdin:
+
+   # Get the words in each line
    words = line.split()
 
+   # Generate the count for each word
    for word in words:
+
+      # Write the key-value pair to STDOUT to be processed by the reducer.
+      # The key is anything before the first tab character and the value is
+      # anything after the first tab character.
       print '{0}\t{1}'.format(word, 1)
--- python/MapReduce/HadoopStreaming/reducer.py
+++ python/MapReduce/HadoopStreaming/reducer.py
@@ -1,21 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import sys
 
 curr_word = None
 curr_count = 0
 
+# Process each key-value pair from the mapper
 for line in sys.stdin:
+
+   # Get the key and value from the current line
    word, count = line.split('\t')
 
+   # Convert the count to an int
    count = int(count)
 
+   # If the current word is the same as the previous word, increment its
+   # count, otherwise print the words count to STDOUT
    if word == curr_word:
       curr_count += count
    else: 
+
+      # Write word and its number of occurrences as a key-value pair to STDOUT
       if curr_word:
          print '{0}\t{1}'.format(curr_word, curr_count)
 
-      curr_count = count
       curr_word = word
+      curr_count = count
 
-print '{0}\t{1}'.format(curr_word, curr_count)
+# Output the count for the last word
+if curr_word == word:
+   print '{0}\t{1}'.format(curr_word, curr_count)
